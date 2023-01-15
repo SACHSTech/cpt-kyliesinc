648e7291a2513e2f70f8ee2148fac3c80be5ab0a
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2023-01-14 22:36:29 -0500

    check ins

--- src/cpt/CovidRecords.java
+++ src/cpt/CovidRecords.java
@@ -1,26 +1,25 @@
 package cpt;
-import java.util.Date;
+import java.time.LocalDate;
 
 public class CovidRecords {
     // instance variables
     String strIsoCode;
     String strContinent;
     String strLocation;
-    Date date;
-    double dbltotalCases;
+    LocalDate   date;
+    double dblTotalCases;
     double dblNewCases;
     double dblTotalCasesPerMil;
     double dblNewCasesPerMil;
 
     // add proper comments still
     // covid records class and getters
-    public CovidRecords(String strIsoCode, String strContinent, String strLocation, Date date, double dbltotalCases, double dblNewCases, double dblTotalCasesPerMil, double dblNewCasesPerMil){
-        
+    public CovidRecords(String strIsoCode, String strContinent, String strLocation, LocalDate date, double dbltotalCases, double dblNewCases, double dblTotalCasesPerMil, double dblNewCasesPerMil){       
         this.strIsoCode = strIsoCode;
         this.strContinent = strContinent;
         this.strLocation = strLocation;
         this.date = date;
-        this.dbltotalCases = dbltotalCases;
+        this.dblTotalCases = dbltotalCases;
         this.dblNewCases = dblNewCases;
         this.dblTotalCasesPerMil = dblTotalCasesPerMil;
         this.dblNewCasesPerMil = dblNewCasesPerMil;
@@ -38,12 +37,12 @@ public class CovidRecords {
         return strLocation;
     }
 
-    public Date getDate(){
+    public LocalDate getDate(){
         return date;
     }
 
     public double getTotalCases(){
-        return dbltotalCases;
+        return dblTotalCases;
     }
 
     public double getNewCases(){
