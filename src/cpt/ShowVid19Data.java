a03329cdc8b6ef1eec232658609af060590ce855
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2023-01-16 19:09:01 -0500

    create method to produce data contient chosen

--- src/cpt/ShowVid19Data.java
+++ src/cpt/ShowVid19Data.java
@@ -6,30 +6,42 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.plaf.basic.BasicTreeUI.SelectionModelPropertyChangeHandler;
-
 public class ShowVid19Data {
     public ShowVid19Data() {
         data = new ArrayList<ShowVid19Record>();
     }
 
+    // add record 
     public void addRecord(ShowVid19Record rec) {
         data.add(rec);
     }
 
+    // dump records
     public void dumpRecords() {
         for(ShowVid19Record rec : data) {
             rec.dump();
         }
     }
 
+    // produce data based on country chosen
     public ShowVid19Data selectByCountry(String country) {
         ShowVid19Data   selection = new ShowVid19Data();
         for (ShowVid19Record rec : data)
         {
-            if (rec.getLocation().equals(country))
+            if (rec.getLocation().equals(country)){
                 selection.addRecord(rec);
-            
+            }          
+        }
+        return selection;
+    }
+
+    // produce data based on contient chosen
+    public ShowVid19Data selectByContinent(String continent) {
+        ShowVid19Data selection = new ShowVid19Data();
+        for (ShowVid19Record rec : data){
+            if(rec.getContinent().equals(continent)){
+                selection.addRecord(rec);
+            }
         }
         return selection;
     }
