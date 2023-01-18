6ba9da1197f2a4c5409fa8ca64fb9473920b6019
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2023-01-17 21:52:44 -0500

    working on contient output

--- src/cpt/ShowVid19Data.java
+++ src/cpt/ShowVid19Data.java
@@ -5,10 +5,13 @@ import java.io.FileReader;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDate;
+
 
 public class ShowVid19Data {
     public ShowVid19Data() {
         data = new ArrayList<ShowVid19Record>();
+        
     }
 
     // add record 
@@ -36,12 +39,74 @@ public class ShowVid19Data {
     }
 
     // produce data based on contient chosen
-    public ShowVid19Data selectByContinent(String continent) {
+    
+    public ShowVidContinent selectByContinent(String continent) {
+        double continentTotalCases = 0.0;
+        LocalDate date = null;
+        ShowVidContinent selection = new ShowVidContinent(continent, date, continentTotalCases);
+
+        for (ShowVidContinent rec : data){
+            if(rec.getContinent().equals(continent)){
+                continentTotalCases += rec.getTotalCases();
+                date = getDate();
+            }
+        }
+        return selection;
+    }
+
+    // produce number of new cases 
+    public ShowVid19Data showNewCases (String newCases){
         ShowVid19Data selection = new ShowVid19Data();
         for (ShowVid19Record rec : data){
-            if(rec.getContinent().equals(continent)){
+            if(rec.getContinent().equals(newCases)){
                 selection.addRecord(rec);
             }
+        }
+        return selection;
+    }
+
+    // produce number of total cases
+    public ShowVid19Data showTotalCases (String totalCases){
+        ShowVid19Data selection = new ShowVid19Data();
+        for (ShowVid19Record rec : data){
+            if(rec.getContinent().equals(totalCases)){
+                selection.addRecord(rec);
+            }
+        }
+        return selection;
+    }
+
+    // just produce the current total case record for each country
+    public ShowVid19Data selectCountryTotals() {
+        ShowVid19Data     selection = new ShowVid19Data();
+        ShowVid19Record   last_rec = null;
+        for (ShowVid19Record rec : data) {
+            if (last_rec != null && !last_rec.getLocation().equals(rec.getLocation())) {
+                    selection.addRecord(last_rec);
+            }
+            last_rec = rec;
+        }
+        if (last_rec != null) {
+            selection.addRecord(last_rec);
+        }
+        return selection;
+    }
+
+    // just produce the current total case record for each continent
+    public ShowVid19Data selectContinentTotals() {
+        ShowVid19Data     selection = new ShowVid19Data();
+        ShowVid19Record   last_rec = null;
+        double            continentTotal = 0.0;
+        for (ShowVid19Record rec : data) {
+            if (last_rec != null && !last_rec.getContinent().equals(rec.getContinent())) {
+                    continentTotal += last_rec.getTotalCases();
+                    
+                    
+            }
+            last_rec = rec;
+        }
+        if (last_rec != null) {
+            selection.addRecord(last_rec);
         }
         return selection;
     }
---
+++ src/cpt/ShowVidContinent.java
@@ -0,0 +1,28 @@
+package cpt;
+import java.time.LocalDate;
+
+
+public class ShowVidContinent {
+
+    String strContinent;
+    LocalDate date;
+    double dblTotalCases;
+
+    public ShowVidContinent (String strContinent, LocalDate date, double dblTotalCases){
+        this.strContinent = strContinent;
+        this.date = date;
+        this.dblTotalCases = dblTotalCases;
+    }
+    
+    public String getContinent(){
+        return strContinent;
+    }
+
+    public LocalDate getDate(){
+        return date;
+    }
+
+    public double getTotalCases(){
+        return dblTotalCases;
+    }
+}
